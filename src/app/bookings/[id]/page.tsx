1e97c3c8c5c647b923e96d4b6adb629bbc4f1fc3
Christopher Peters <petersc@nskind.com>
2026-06-11 07:19:33 -0400

    searching for booking.umpire references

--- src/app/bookings/[id]/page.tsx
+++ src/app/bookings/[id]/page.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import { prisma } from "@/lib/prisma";
-import DeleteBookingButton from "@/components/DeleteBookingButton";
+import DeleteBookingButton from "@/components/delete-booking-button";
 
 type PageProps = {
   params: Promise<{ id: string }>;
